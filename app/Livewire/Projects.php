04dbf865c7a96ab3c41be38e6a9c9d14de8f1cf7
Giovanni Kuan <giokuan@gmail.com>
2024-07-20 22:22:42 +0800

    revised the project page

--- app/Livewire/Projects.php
+++ app/Livewire/Projects.php
@@ -11,6 +11,38 @@ class Projects extends Component
     public bool $myPersistentModal3 = false;
     public bool $myPersistentModal4 = false;
 
+    public $barangayrecordsystem = 'https://youtu.be/rp-VY-y1oGs?si=ZR80LRcV-a7EO-M5';
+
+    public $sjbmc = 'https://youtu.be/QpB0usKbhTU?si=PDaiyUSwpdhR_XRI';
+
+    public $lazy = 'https://youtu.be/n3ShQvM3do8?si=VaJs_pM4tlfqFmEL';
+
+    public $portal = 'https://youtu.be/2jMDMhAPTjk?si=ctrI3splX1jPJD9Y';
+
+
+
+
+    public function redirectToVideoLink()
+    {
+        return redirect()->to($this->barangayrecordsystem);
+    }
+
+    public function SJBMCVideo()
+    {
+        return redirect()->to($this->sjbmc);
+    }
+
+    public function LazyReader()
+    {
+        return redirect()->to($this->lazy);
+    }
+
+    public function TINHSPortal()
+    {
+        return redirect()->to($this->portal);
+    }
+
+
     public function openModal()
     {
         $this->myPersistentModal = true;
