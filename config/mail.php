19dbb51d006272710b02ec4d03161bec88bcdd03
Giovanni Kuan <giokuan@gmail.com>
2025-02-28 18:50:37 +0800

    change mail server to gmail

--- config/mail.php
+++ config/mail.php
@@ -48,17 +48,30 @@ return [
 
  
 
-    'mailers' => [
+   'mailers' => [
         'smtp' => [
+            'transport' => 'smtp',
+            'url' => env('MAIL_URL'),
+            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'port' => env('MAIL_PORT', 587),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
+            'timeout' => null,
+            'local_domain' => env('MAIL_EHLO_DOMAIN'),
+        ],
+
+    'gmail' => [
         'transport' => 'smtp',
-        'host' => env('MAIL_HOST', 'smtp.gmail.com'), 
-        'port' => env('MAIL_PORT', 465),
-        'encryption' => env('MAIL_ENCRYPTION', 'tsl'),
+        'host' => 'smtp.gmail.com',
+        'port' => 587,
+        'encryption' => 'tls',
         'username' => env('MAIL_USERNAME'),
         'password' => env('MAIL_PASSWORD'),
         'timeout' => null,
         'local_domain' => env('MAIL_EHLO_DOMAIN'),
     ],
+
 
         'ses' => [
             'transport' => 'ses',
